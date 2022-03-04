8a6a1ccc8f22c31539fbfa57dc911f9e8720927f
eval-exec <execvy@gmail.com>
2022-03-04 22:50:24 +0800

    remove useless pkg

--- bin/src/game_2d.rs
+++ bin/src/game_2d.rs
@@ -94,6 +94,20 @@ impl Board {
     }
 }
 
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(3, add_one(2));
+    }
+}
+
 struct Universe {
     twin: Vec<Board>,
     iboard: usize,
